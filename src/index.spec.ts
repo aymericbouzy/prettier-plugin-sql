ec404638fdbd988b819bd31232e1e6a2467fb874
aymeric bouzy <8168981+aymericbouzy@users.noreply.github.com>
2022-03-06 17:21:14 +0100

    feat: format simple query

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,18 @@
-import { add } from ".";
+import path from "path";
+import prettier from "prettier";
 
-describe("add", () => {
-  it("adds two numbers", () => {
-    expect(add(1, 2)).toBe(3);
-  });
+const query =
+	"SELECT * from users u join users_roles ur on ur.user_id = u.id where ur.role = 'admin'";
+
+it("formats query", () => {
+	expect(
+		prettier.format(query, {
+			parser: "sql",
+			plugins: [path.dirname(__dirname)],
+		})
+	).toBe(`select *
+from users u
+join users_roles ur on ur.user_id = u.id
+where ur.role = "admin";
+`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,112 @@
-export function add(a: number, b: number) {
-  return a + b;
+import { Plugin, doc, Doc } from "prettier";
+import { Parser } from "node-sql-parser";
+
+export const languages: Plugin["languages"] = [
+	{
+		name: "SQL",
+		parsers: ["sql"],
+	},
+];
+
+const parser = new Parser();
+
+export const parsers: Plugin["parsers"] = {
+	sql: {
+		parse(text) {
+			return parser.astify(text);
+		},
+		astFormat: "sql",
+		locStart() {
+			return 0;
+		},
+		locEnd() {
+			return 0;
+		},
+	},
+};
+
+const { join, group, ifBreak, indent, softline, line } = doc.builders;
+
+interface FromInitial {
+	db: null | string;
+	table: string;
+	as: string;
+	join: undefined;
 }
+
+interface FromJoin extends Omit<FromInitial, "join"> {
+	join: string;
+	on: any;
+}
+
+type From = FromInitial | FromJoin;
+
+function print(node: any): Doc {
+	if (node.type === "column_ref") {
+		return join(".", [node.table, node.column]);
+	}
+
+	if (node.type === "single_quote_string") {
+		return ['"', node.value, '"'];
+	}
+
+	if (node.type === "binary_expr") {
+		return group([
+			print(node.left),
+			" ",
+			node.operator,
+			" ",
+			print(node.right),
+		]);
+	}
+
+	if (node.type === "select") {
+		return group([
+			join(" ", ["select", node.columns]),
+			softline,
+			...node.from.map((from: From) => {
+				if (from.join) {
+					const on = join(" ", ["on", print(from.on)]);
+
+					return [
+						group([
+							join(" ", [
+								from.join === "INNER JOIN" ? "join" : from.join.toLowerCase(),
+								from.db ? `\`${from.db}\`.${from.table}` : from.table,
+								from.as,
+								ifBreak(indent(on), on),
+							]),
+						]),
+						softline,
+					];
+				}
+
+				return [
+					group([
+						join(" ", [
+							"from",
+							from.db ? `\`${from.db}\`.${from.table}` : from.table,
+							from.as,
+						]),
+					]),
+					softline,
+				];
+			}),
+			join(" ", ["where", print(node.where)]),
+			";",
+			line,
+		]);
+	}
+
+	return " ";
+}
+
+export const printers: Plugin["printers"] = {
+	sql: {
+		print(path) {
+			const node = path.getValue();
+
+			return print(node);
+		},
+	},
+};
